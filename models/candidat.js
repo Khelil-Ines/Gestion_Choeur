893a6c5f64830a805213a72f602958a55913ea87
Ines Khelil <khelilines14@gmail.com>
2023-11-18 21:06:57 +0100

    first concert

--- models/candidat.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const candidatSchema = mongoose.Schema({
-  nom:{type :String, required:true},
-  prenom:{type :String, required:true}
-
-});
-module.exports = mongoose.model("Candidat", candidatSchema);
