d2c9b7452d798a3663792e97876636446894747b
Ines Khelil <khelilines14@gmail.com>
2023-12-09 16:59:27 +0100

    latest modif

--- controllers/programme.js
+++ controllers/programme.js
@@ -1,31 +1,31 @@
 const Programme = require("../models/programme.js");
-const excelToJson = require('convert-excel-to-json');
+const Oeuvre = require("../models/oeuvre.js");
+const excelToJson = require("convert-excel-to-json");
 const addProgramme = (req, res) => {
   Programme.create(req.body)
-   .then((programme) => {
+    .then((programme) => {
       res.status(201).json({
         model: programme,
         message: "Programme crée!",
       });
     })
-   .catch((error) => res.status(400).json({ error }));
+    .catch((error) => res.status(400).json({ error }));
 };
 
 // const addProgrammewithFile = (req, res) => {
 
-
 //     if (!req.file) {
 //       return res.status(400).json({ message: 'Aucun fichier téléchargé' });
 //     }
-  
+
 //     // Convertir le fichier Excel en JSON
 //     const excelData = excelToJson({
 //       sourceFile: Buffer.from(req.file.buffer).toString('utf8')
 //     });
-  
+
 //     // En supposant que votre fichier Excel a une structure spécifique, ajustez le code en conséquence
 //     const programmeData = excelData.sheet1; // Ajustez le nom de la feuille si nécessaire
-  
+
 //     Programme.create(programmeData)
 //       .then((programme) => {
 //         res.status(201).json({
@@ -117,72 +117,112 @@ const addProgramme = (req, res) => {
 //     res.status(500).json({ error: 'Erreur lors de la création des programmes' });
 //   }
 // };
-const xlsx = require('xlsx')
-const addProgrammewithFile = async (req, res) => { 
-
+const xlsx = require("xlsx");
+const addProgrammewithFile = async (req, res) => {
   try {
-      console.log("req.file", req.file);
-      console.log("req.body",req.body)
-      if (!req.file) {
-        return res.status(400).json({ message: 'Aucun fichier téléchargé' });
-      }
-      
-      // Convertir le buffer du fichier Excel en un objet JSON
-      const workbook = xlsx.read(req.file.buffer, { type: 'buffer' });
-      const sheetName = workbook.SheetNames[0]; 
-     
-      if (!sheetName) {
-        return res.status(400).json({ message: 'Le fichier Excel ne contient pas de feuille valide' });
-      }
-   
-      const excelData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
-    
-      if (!excelData || excelData.length === 0) {
-        return res.status(400).json({ message: 'Le fichier Excel ne contient pas de données valides' });
-      }
-    
-      // Parcourir les données Excel et créer un document Programme pour chaque ligne
-      const programmes = excelData.map((row) => {
-        return new Programme({
-          theme: row.theme,
-          oeuvre: row.oeuvre, // Référence à l'ID de l'oeuvre
-        });
-      });
-    
-      if (programmes.length === 0) {
-        return res.status(400).json({ message: 'Aucun programme trouvé dans le fichier Excel' });
-      }
-    
-      // Sauvegarder tous les programmes en une seule fois
-      const savedProgrammes = await Programme.insertMany(programmes);
-    
-      res.status(201).json({
-        message: 'Programmes créés à partir du fichier Excel',
-        programmes: savedProgrammes,
-      });
-    } catch (error) {
-      console.error('Erreur lors de la création des programmes :', error);
-      res.status(500).json({ error: 'Erreur lors de la création des programmes' });
+    console.log("req.file", req.file);
+    console.log("req.body", req.body);
+    if (!req.file) {
+      return res.status(400).json({ message: "Aucun fichier téléchargé" });
     }
-}
 
+    // Convertir le buffer du fichier Excel en un objet JSON
+    const workbook = xlsx.read(req.file.buffer, { type: "buffer" });
+    const sheetName = workbook.SheetNames[0];
 
+    if (!sheetName) {
+      return res
+        .status(400)
+        .json({
+          message: "Le fichier Excel ne contient pas de feuille valide",
+        });
+    }
 
+    const excelData = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
+
+    if (!excelData || excelData.length === 0) {
+      return res
+        .status(400)
+        .json({
+          message: "Le fichier Excel ne contient pas de données valides",
+        });
+    }
+
+    const savedProgrammes = [];
+    const savedOeuvres = [];
+
+    for (const row of excelData) {
+      // Check if a Programme with the same theme already exists
+      const existingProgramme = await Programme.findOne({ theme: row.theme });
+
+      if (existingProgramme) {
+        // If it exists, update or add the Oeuvre
+        const savedOeuvre = await Oeuvre.findOneAndUpdate(
+          { titre: row.oeuvre },
+          { $addToSet: { choral: row.choriste } },
+          { upsert: true, new: true }
+        );
+
+        // Update the Programme to include the Oeuvre if it's not already included
+        if (!existingProgramme.oeuvre.includes(savedOeuvre._id)) {
+          existingProgramme.oeuvre.push(savedOeuvre._id);
+          await existingProgramme.save();
+        }
+
+        savedProgrammes.push(existingProgramme);
+        savedOeuvres.push(savedOeuvre);
+      } else {
+        // If the Programme doesn't exist, create both Programme and Oeuvre
+        const newOeuvre = new Oeuvre({
+          titre: row.oeuvre,
+          choral: row.choriste,
+        });
+
+        const savedOeuvre = await newOeuvre.save();
+
+        const newProgramme = new Programme({
+          theme: row.theme,
+          oeuvre: [savedOeuvre._id], // Store Oeuvre IDs as an array
+        });
+
+        const savedProgramme = await newProgramme.save();
+
+        savedProgrammes.push(savedProgramme);
+        savedOeuvres.push(savedOeuvre);
+      }
+    }
+
+    res.status(201).json({
+      message: "Programmes et Oeuvres créés à partir du fichier Excel",
+      programmes: savedProgrammes,
+      oeuvres: savedOeuvres,
+    });
+  } catch (error) {
+    console.error(
+      "Erreur lors de la création des programmes et oeuvres :",
+      error
+    );
+    res
+      .status(500)
+      .json({ error: "Programmes et Oeuvres déja existants !" });
+  }
+};
 
 const getProgrammes = (req, res) => {
-    Programme.find().populate("oeuvre")
-     .then((programmes) => {
-        res.status(200).json(programmes);
-      })
-     .catch((error) => {
-        res.status(400).json({
-          error: error,
-        });
+  Programme.find()
+    .populate("oeuvre")
+    .then((programmes) => {
+      res.status(200).json(programmes);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        error: error,
       });
-  }
+    });
+};
 
-  module.exports = {
-    addProgramme,
-    getProgrammes,
-    addProgrammewithFile
-  };
+module.exports = {
+  addProgramme,
+  getProgrammes,
+  addProgrammewithFile,
+};
--- models/oeuvre.js
+++ models/oeuvre.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Oeuvre = mongoose.model('oeuvre', {
     // affiche (optionnel) 
- 
+ titre : { type : String, required : true},
   choral: {
     type: Boolean,
     required: true
