b330e6a7ad783efe9a7a9646b238a737aed3797c
asmabouziri <asmabouziri299@gmail.com>
2023-12-18 13:45:55 +0100

    modifier schema

--- models/oeuvre.js
+++ models/oeuvre.js
@@ -9,14 +9,7 @@ const oeuvreSchema = mongoose.Schema({
   partition: { type: String, required: false },
   paroles: { type: String, required: false },
   presenceChoeur: { type: Boolean, required: true },
-  parties: [
-    {
-      numero: { type: Number, required: true },
-      nom: { type: String, required: true },
-      partieChoeur: { type: Boolean, required: false },
-      pupitres: [{ type: String, required: false }],
-    },
-  ],
+
 });
 
 module.exports = mongoose.model("Oeuvre", oeuvreSchema);
