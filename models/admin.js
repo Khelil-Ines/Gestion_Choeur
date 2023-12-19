0fc7137e4d8143453280d6ea3603e7014794067a
Ines Khelil <khelilines14@gmail.com>
2023-12-19 15:41:38 +0100

    modif

--- models/admin.js
+++ models/admin.js
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const utilisateur = require("./utilisateur");
 
-const AdminSchema = mongoose.Schema({
-
-});
-
-module.exports = utilisateur.discriminator("Admin", AdminSchema);
-=======
 const mongoose = require ("mongoose");
 const utilisateur = require("./utilisateur");
 
@@ -17,4 +8,4 @@ const adminSchema = mongoose.Schema(
 )
 module.exports = utilisateur.discriminator('Admin', adminSchema);
 
->>>>>>> Statut_choriste
+
