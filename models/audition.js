3252d1034babfe818e810ab9e00972b4f786aa1e
Ines Khelil <khelilines14@gmail.com>
2023-12-19 13:21:36 +0100

    schemas

--- models/audition.js
+++ models/audition.js
@@ -8,14 +8,15 @@ const auditionSchema = mongoose.Schema({
     appréciation :{type : String,  enum: ["A+","A", "A-","B+", "B", "B-","C+", "C", "C-"]},
     remarque : {type : String, default: ""},
     présence : {type:Boolean,default:false},
-    candidates: [
-        {
-          candidateId: { type: Schema.Types.ObjectId, ref: 'Candidat', required: true },
-          auditionStartTime: { type: String, required: true },
-          auditionDuration: { type: Number, required: true },
-        }
-      ]
-});
-module.exports = mongoose.model("Audition", auditionSchema);
+    candidat: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Candidat',
+      required: true,
+    },
+    dateAudition: { type: Date, required: true },
+    HeureDeb: { type: String, required: true },
+    HeureFin: { type: String, required: true },
+  
+  });
 
- 
+module.exports = mongoose.model("Audition", auditionSchema);
