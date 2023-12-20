406f3eb1357c0fc79a812703feea3bbbbb5b2a0e
asmabouziri <asmabouziri299@gmail.com>
2023-12-20 23:34:34 +0100

    cron jobs

--- controllers/notifrepetition.js
+++ controllers/notifrepetition.js
@@ -1,29 +1,36 @@
 const cron = require("node-cron");
 const choriste = require("../models/choriste");
 
+let notif;
 
 const envoyerNotification = (req, res) => {
-  let count = 0;
   const repetitions = req.params.repetitions;
-  let notif = cron.schedule(
+  
+  if (notif) {
+    notif.stop();
+  }
+
+  let count = 0;
+  notif = cron.schedule(
     "* * * * * *",
     () => {
       count++;
       console.log(
         `Rappelez-vous ! vous avez une répétition le ..... (fois ${count}/${repetitions})`
       );
-      if (count === repetitions) {
-        notif.stop(); // Arrête la tâche une fois que le nombre de répétitions est atteint
+      if (count >= repetitions) {
+        notif.stop(); 
         count = 0;
       }
     },
     {
-      scheduled: false, // Empêche l'exécution immédiate de la tâche après la planification
+      scheduled: false, 
     }
   );
-  notif.start();
+
+  notif.start(); 
 };
 
 module.exports = {
-  envoyerNotification,
+  envoyerNotification
 };
