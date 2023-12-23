1cfedb7125faa136a9cc58ab81d5f5a5d2cc03b9
Ines Khelil <khelilines14@gmail.com>
2023-12-23 13:13:56 +0100

    package

--- models/candidat.js
+++ models/candidat.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 const moment = require('moment');
 const utilisateur = require("./utilisateur");
-<<<<<<< HEAD
 
 const CandidatSchema = mongoose.Schema({
   nom: { type: String, required: true },
   prenom: { type: String, required: true },
   email: { type: String, required: true },
-=======
-const CandidatSchema = mongoose.Schema({
->>>>>>> c12b5b3d2125d83db676d5dbf22aaa4617d8c57e
   connaissance_musicale: { type: String, required: true },
   autres_activites: { type: Boolean, required: true },
   Taille: { type: Number, required: true },
