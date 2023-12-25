947701b89795de97c498b92873c49e1470552020
GhofraneMannai <ghofranemanai2019@gmail.com>
2023-12-25 21:40:45 +0100

    merge_main

--- routes/audition.js
+++ routes/audition.js
@@ -5,20 +5,20 @@ const planningController = require('../controllers/audition');
 
 
 router.post('/generate/:startDate/:sessionStartTime/:sessionEndTime/:candidatesPerHour', planningController.genererPlanning);
-router.get('/fetch', planningController.fetchPlanning);
-router.get('/fetchDateHeure', planningController.fetchPlanningByDateHeure);
-router.get('/name', planningController.fetchPlanningByCandidat);
-router.post("/", planningController.addAudition);
-router.delete("/:id", planningController.deleteAudition);
-router.patch("/:id", planningController.updateAudition);
-router.get("/", planningController.getAudition);
-router.get("/:id", planningController.fetchAudition);
-router.get("/candidats/:filtre", planningController.getCandidatsFiltres);
-router.post("/liste", planningController.getCandidatPupitreOrdonnes);
-router.post("/email-acceptation/:id", planningController.envoyerEmailAcceptation);
-router.use("/confirmationCandidat/:id", planningController.confirmationCandidat);
-router.get('/candidat/:candidatId', planningController.fetchPlanningByid);
-router.post('/defaillant', planningController.genererPlanningDefaillants);
+// router.get('/fetch', planningController.fetchPlanning);
+// router.get('/fetchDateHeure', planningController.fetchPlanningByDateHeure);
+// router.get('/name', planningController.fetchPlanningByCandidat);
+// router.post("/", planningController.addAudition);
+// router.delete("/:id", planningController.deleteAudition);
+// router.patch("/:id", planningController.updateAudition);
+// router.get("/", planningController.getAudition);
+// router.get("/:id", planningController.fetchAudition);
+// router.get("/candidats/:filtre", planningController.getCandidatsFiltres);
+// router.post("/liste", planningController.getCandidatPupitreOrdonnes);
+// router.post("/email-acceptation/:id", planningController.envoyerEmailAcceptation);
+// router.use("/confirmationCandidat/:id", planningController.confirmationCandidat);
+// router.get('/candidat/:candidatId', planningController.fetchPlanningByid);
+// router.post('/defaillant', planningController.genererPlanningDefaillants);
 
 
 
