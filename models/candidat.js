3c3f1a799c750a3ce99c16551194bbd42054a105
asmabouziri <asmabouziri299@gmail.com>
2024-01-16 21:48:32 +0100

    updates

--- models/candidat.js
+++ models/candidat.js
@@ -6,7 +6,7 @@ const utilisateur = require("./utilisateur");
 const CandidatSchema = mongoose.Schema({
   nom: { type: String, required: true },
   prénom: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true , unique:true },
   connaissance_musicale: { type: String, required: true },
   autres_activites: { type: Boolean, required: true },
   Taille: { type: Number, required: true },
